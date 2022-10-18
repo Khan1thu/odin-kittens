827275abadaad70c847d7ad52be2b62251f769f8
Khan1thu <bienew311@gmail.com>
2022-10-18 13:53:49 +0630

    finished creating kitten

--- app/controllers/kitten_controller.rb
+++ app/controllers/kitten_controller.rb
@@ -1,4 +1,5 @@
 class KittenController < ApplicationController
+
     def index
         @kitten = Kitten.all
     end
@@ -6,4 +7,24 @@ class KittenController < ApplicationController
     def show
         @k = Kitten.find(params[:id])
     end
-end
+
+    def new
+        @kitten = Kitten.new
+    end
+
+    def create
+        @kitten = Kitten.new(kitten_params) 
+        if @kitten.save
+            flash.alert = "You saved!"
+            redirect_to "/"
+        else
+            flash.alert = "Failed"
+        end
+    end
+
+    
+    private
+        def kitten_params
+            params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+        end
+    end
