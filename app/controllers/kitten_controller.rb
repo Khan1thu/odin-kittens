1ff0a5bb9e0cd44c50411058fcc639550b053ab8
Khan1thu <bienew311@gmail.com>
2022-10-18 13:58:42 +0630

    Finished updating the kitten

--- app/controllers/kitten_controller.rb
+++ app/controllers/kitten_controller.rb
@@ -16,12 +16,27 @@ class KittenController < ApplicationController
         @kitten = Kitten.new(kitten_params) 
         if @kitten.save
             flash.alert = "You saved!"
-            redirect_to "/"
+            redirect_to @kitten
         else
             flash.alert = "Failed"
         end
     end
 
+    def edit
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def update
+        @kitten = Kitten.find(params[:id])
+
+        if @kitten.update(kitten_params)
+          flash[:success] = "Your kitten has been updated!"
+          redirect_to @kitten
+        else
+          flash.now[:error] = "The kitten couldn't be updated, please fix your errors."
+          render :edit, status: :unprocessable_entity
+        end
+    end
     
     private
         def kitten_params
